b70390945e58fcac5821ed9c9b33b04e09e281b2
Facundo Sapienza <fsapienza@berkeley.edu>
2021-11-17 10:39:42 +0100

    Auxiliar functions

---
+++ scripts/auxiliar.py
@@ -0,0 +1,26 @@
+import numpy as np
+import os
+
+
+def get_files_in_directory(path): 
+    """
+    Retrieves file names from a directory \
+    \n\nInput: path = directory \
+    \n\nOutput: list of subdirectories
+    """
+
+    # The last conditional here is in order to ignore the /DS_store file in macs 
+    return [os.path.join(path, name) for name in os.listdir(path)
+            if (os.path.isfile(os.path.join(path, name)) and (not name.startswith('.')))  ]
+
+def spherical2cartesian(theta, phi):
+    """
+    v[0] = theta / Latitude
+    v[1] = phi   / Longitude
+    """
+    
+    x = np.cos(theta) * np.cos(phi)  
+    y = np.cos(theta) * np.sin(phi)  
+    z = np.sin(theta)  
+    
+    return x, y, z
